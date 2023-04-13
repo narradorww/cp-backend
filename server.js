8ef78794c93b8645fdf22a985c6591f67f244a89
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2023-04-13 17:22:41 -0300

    Implementada rota de autenticação e de registro para estrategia local

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import passport from 'passport';
-import passportGoogle from 'passport-google-oauth20';
-import pkg from 'passport-facebook-token';
 import dotenv from 'dotenv';
 import recipeRoutes from './routes/recipeRoutes.js';
 import bakerRoutes from './routes/bakerRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import donationRoutes from './routes/donationRoutes.js';
-
-const { Strategy: FacebookTokenStrategy } = pkg;
-const { Strategy: GoogleStrategy } = passportGoogle;
+import authRouter from './routes/authRouter.js';
+import auth from './config/auth.js';
 
 // Load environment variables
 dotenv.config();
@@ -22,46 +18,26 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(passport.initialize());
-
-// Passport configuration
-// Google OAuth
-passport.use(
-    new GoogleStrategy(
-      {
-        clientID: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback",
-        authorizationURL: "https://accounts.google.com/o/oauth2/v2/auth",
-        tokenURL: "https://oauth2.googleapis.com/token",
-      },
-      async (accessToken, refreshToken, profile, done) => {
-        // Aqui você pode implementar a lógica para vincular o perfil do Google ao usuário no seu banco de dados
-      }
-    )
-  )
-
-// Facebook Token
-passport.use(
-    new FacebookTokenStrategy(
-      {
-        clientID: process.env.FACEBOOK_APP_ID,
-        clientSecret: process.env.FACEBOOK_APP_SECRET,
-        authorizationURL: "https://www.facebook.com/dialog/oauth",
-        tokenURL: "https://graph.facebook.com/oauth/access_token",
-      },
-      async (accessToken, refreshToken, profile, done) => {
-        // Aqui você pode implementar a lógica para vincular o perfil do Facebook ao usuário no seu banco de dados
-      }
-    )
-  );
+app.use(auth.initialize());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
+// Authentication routes
+app.get('/auth/google', auth.authenticate('google', { scope: ['profile', 'email'] }));
+app.get('/auth/google/callback', auth.authenticate('google'), (req, res) => {
+  // Handle successful authentication here
+});
+
+app.get('/auth/facebook', auth.authenticate('facebook', { scope: ['email'] }));
+app.get('/auth/facebook/callback', auth.authenticate('facebook'), (req, res) => {
+  // Handle successful authentication here
+});
+
 // Routes
+app.use('/api/auth', authRouter);
 app.use('/api/recipes', recipeRoutes);
 app.use('/api/bakers', bakerRoutes);
 app.use('/api/posts', postRoutes);
